3364b1bd93317cf4862dedf7afab90fbb94f1208
DeependraParichha1004 <deependragit15@gmail.com>
2023-07-04 21:23:24 +0500

    Merge Sort added

--- code/Sorting.cpp
+++ code/Sorting.cpp
@@ -164,6 +164,77 @@ void insertion_sort(int arr[],int n){
     }
 }
 
+//Merge Sort 
+void merge(int *arr, int s, int e) {
+
+    int mid = (s+e)/2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    //copy values
+    int mainArrayIndex = s;
+    for(int i=0; i<len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid+1;
+    for(int i=0; i<len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    //merge 2 sorted arrays     
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while(index1 < len1 && index2 < len2) {
+        if(first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else{
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }   
+
+    while(index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while(index2 < len2 ) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+    cout<<"merge";
+
+}
+
+
+void mergeSort(int *arr, int s, int e) {
+    //base case
+    if(s >= e) {
+        return;
+    }
+    
+    int mid = (s+e)/2;
+
+    //left part sort karna h 
+    mergeSort(arr, s, mid);
+    
+    //right part sort karna h 
+    mergeSort(arr, mid+1, e);
+
+    //merge
+    merge(arr, s, e);
+    
+
+}
+
 int main()
 {
     // (1) Selection Sort
@@ -207,5 +278,17 @@ int main()
     // for(int i=0;i<n;i++){
     //     cout<<arr[i]<<" ";
     // }    
+
+    //(6) Merge Sort 
+    // int arr[] = {2,1,6,4,18,9,17,5};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+
+    // mergeSort(arr, 0, n-1);
+
+    // for(int i=0;i<n;i++){
+    //     cout << arr[i] << " ";
+    // }
+
+    
     return 0;
 }
