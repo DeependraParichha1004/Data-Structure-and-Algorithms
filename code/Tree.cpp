fd64ce1b58eb85e1c525d1128a292d3d43130fb5
DeependraParichha1004 <deependragit15@gmail.com>
2023-08-17 16:48:25 +0500

    Inorder Traversal

--- code/Tree.cpp
+++ code/Tree.cpp
@@ -39,11 +39,58 @@ BinaryTree* Tree(BinaryTree* root){
     cout<<"enter data for inserting in right of: "<<data<<endl;
     root->right=Tree(root->right);
     return root;
+}
+
+void level_order_traversal(BinaryTree* root){
+    queue<BinaryTree*> q;
+    q.push(root);
+    q.push(NULL);//separator
+
+    while(!q.empty()){
+        BinaryTree* temp=q.front();
+        
+        q.pop();
+
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }  
+    }
+}
+
+void inorder_traversal(BinaryTree* root){
+    BinaryTree* temp=root;
+    if(temp->left==NULL){
+        cout<<temp->data;
+        return ;
+    }
+    if(temp->right==NULL){
+        cout<<temp->data;
+        return ;
+    }
+
+    inorder_traversal(temp->left);
+    cout<<temp->data;
+    inorder_traversal(temp->right);
+
 
 }
+
 int main(){
    BinaryTree* root=NULL;
-   Tree(root);
-     
-    return 0; 
+   root=Tree(root);
+//    level_order_traversal(root);
+   inorder_traversal(root);
+   return 0; 
 }
